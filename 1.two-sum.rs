0aba96a1b9c2fc05421212d12edd6c3607e2f79c
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-02-14 22:10:12 +0900

    Add 1.two-sum.rs

---
+++ 1.two-sum.rs
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=rust
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in (i + 1)..nums.len() {
+                if nums[i] + nums[j] == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        unreachable!()
+    }
+}
+// @lc code=end
