de009bd63087d2cb9eca010f8fe95964d5bd8f69
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-03-12 00:13:05 +0900

    Fix 等比数列

--- AtCoder/等比数列/my.ts
+++ AtCoder/等比数列/my.ts
@@ -1,6 +1,10 @@
-const A = 2;
-const R = 3;
-const N = 4;
+import * as fs from "fs";
+
+const [A, R, N] = fs.readFileSync("/dev/stdin", "utf8").split(" ").map(Number);
+
+// const A = 2;
+// const R = 3;
+// const N = 4;
 
 const solve = (A: number, R: number, N: number): number | "large" => {
   for (let i = 0; i < N - 1; i++) {
