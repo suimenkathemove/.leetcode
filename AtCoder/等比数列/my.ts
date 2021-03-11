51fadf37e60e6b6cb8bbb4a574cebd5095de235e
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-03-11 10:14:10 +0900

    Add solve to 等比数列

--- AtCoder/等比数列/my.ts
+++ AtCoder/等比数列/my.ts
@@ -2,10 +2,18 @@ const A = 2;
 const R = 3;
 const N = 4;
 
-const ans = A * R ** (N - 1);
+const solve = (A: number, R: number, N: number): number | "large" => {
+  for (let i = 0; i < N - 1; i++) {
+    A *= R;
 
-if (ans > 10 ** 9) {
-  console.log("large");
-} else {
-  console.log(ans);
-}
+    if (A > 10 ** 9) {
+      return "large";
+    }
+  }
+
+  return A;
+};
+
+const ans = solve(A, R, N);
+
+console.log(ans);
