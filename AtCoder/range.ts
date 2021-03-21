5ec0c673ecf5c63546a4842300db0e0ec27a72a5
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-03-21 15:10:29 +0900

    Fix range.ts

--- AtCoder/range.ts
+++ AtCoder/range.ts
@@ -1,4 +1,6 @@
-function* range(start: number, end: number) {
+function* range(...args: [number, number] | [number]) {
+  const [start, end] = args.length === 2 ? args : [0, ...args];
+
   for (let i = start; i < end; i++) {
     yield i;
   }
