c233a9fe4dac1cf67b215255b4cc536b65bea4b8
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-03-31 12:49:55 +0900

    Fix 1-9Grid/ans.ts

--- AtCoder/1-9Grid/ans.ts
+++ AtCoder/1-9Grid/ans.ts
@@ -1,73 +1,72 @@
-import * as fs from "fs";
+const range = (
+  ...args: [end: number] | [start: number, end: number, step?: number]
+): number[] => {
+  const arr: number[] = [];
 
-const input = fs.readFileSync("/dev/stdin", "utf8");
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  let i = start;
+  while (step > 0 ? i < end : i > end) {
+    arr.push(i);
+    i += step;
+  }
+
+  return arr;
+};
+
+const main = (lines: string[]): void => {
+  const [N, M] = lines[0].split(" ").map(Number);
+  const A = lines.slice(1).map((str) => str.split(""));
+
+  const group: [number, number][][] = range(11).map(() => []);
+  range(N).forEach((i) => {
+    range(M).forEach((j) => {
+      const c = A[i][j];
+
+      switch (c) {
+        case "S":
+          group[0].push([i, j]);
+          break;
+        case "G":
+          group[10].push([i, j]);
+          break;
+        default:
+          group[Number(c)].push([i, j]);
+      }
+    });
+  });
+
+  const INF = 10 ** 100;
+  const cost: number[][] = range(N).map(() => range(M).map(() => INF));
+
+  const [si, sj] = group[0][0];
+  cost[si][sj] = 0;
+
+  range(1, 11).forEach((n) => {
+    group[n].forEach(([i, j]) => {
+      group[n - 1].forEach(([i2, j2]) => {
+        cost[i][j] = Math.min(
+          cost[i][j],
+          cost[i2][j2] + Math.abs(i - i2) + Math.abs(j - j2)
+        );
+      });
+    });
+  });
+
+  const [gi, gj] = group[10][0];
+  const ans = cost[gi][gj] !== INF ? cost[gi][gj] : -1;
+
+  console.log(ans);
+};
+
 // const input = `3 4
 // 1S23
 // 4567
 // 89G1`;
-
-const splitInput = input.split(`\n`);
-
-const [N, M] = splitInput[0].split(" ").map(Number);
-
-const A = splitInput.slice(1).map((str) => str.split(""));
-
-const group: [number, number][][] = (() => {
-  const arr: [number, number][][] = (() => {
-    const arr: any[][][] = [];
-    for (let i = 0; i < 11; i++) {
-      arr.push([]);
-    }
-    return arr as [number, number][][];
-  })();
-
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      switch (A[i][j]) {
-        case "S":
-          arr[0].push([i, j]);
-          break;
-        case "G":
-          arr[10].push([i, j]);
-          break;
-        default:
-          arr[Number(A[i][j])].push([i, j]);
-      }
-    }
-  }
-
-  return arr;
-})();
-
-const INF = 10 ** 100;
-
-const cost = (() => {
-  const arr: number[][] = [];
-  for (let i = 0; i < N; i++) {
-    const arr2 = [];
-    for (let j = 0; j < M; j++) {
-      arr2.push(INF);
-    }
-    arr.push(arr2);
-  }
-  return arr;
-})();
-
-const [si, sj] = group[0][0];
-cost[si][sj] = 0;
-
-for (let n = 1; n < 11; n++) {
-  for (let [i, j] of group[n]) {
-    for (let [i2, j2] of group[n - 1]) {
-      cost[i][j] = Math.min(
-        cost[i][j],
-        cost[i2][j2] + Math.abs(i - i2) + Math.abs(j - j2)
-      );
-    }
-  }
-}
-
-const [gi, gj] = group[10][0];
-const ans = cost[gi][gj];
-
-console.log(ans !== INF ? ans : -1);
+// export const mainReturn = main(input.split("\n"));
+main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
