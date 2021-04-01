53c38c4a066629aa9847f1b3fb9fb48931582986
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-01 17:12:32 +0900

    Fix 組分け/ans.ts

--- AtCoder/組分け/ans.ts
+++ AtCoder/組分け/ans.ts
@@ -1,56 +1,70 @@
-function* range(...args: [number, number] | [number]) {
-  const [start, end] = args.length === 2 ? args : [0, ...args];
+const range = (
+  ...args: [end: number] | [start: number, end: number, step?: number]
+): number[] => {
+  const arr: number[] = [];
 
-  for (let i = start; i < end; i++) {
-    yield i;
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  let i = start;
+  while (step > 0 ? i < end : i > end) {
+    arr.push(i);
+    i += step;
   }
-}
 
-import * as fs from "fs";
+  return arr;
+};
 
-const input = fs.readFileSync("/dev/stdin", "utf8");
-// const input = `6
-// 10 10 -10 -10 -10
-// 10 -10 -10 -10
-// -10 -10 -10
-// 10 -10
-// -10`;
+const main = (lines: string[]): void => {
+  const N = Number(lines[0]);
 
-const splitInput = input.split("\n");
+  const A: number[][] = [];
+  range(N - 1).forEach((i) => {
+    A.push([
+      ...range(i + 1).map(() => 0),
+      ...lines.slice(1)[i].split(" ").map(Number),
+    ]);
+  });
 
-const N = Number(splitInput[0]);
+  const ALL = 1 << N;
 
-const A = [...range(N - 1)].map((i) => [
-  ...[...range(i + 1)].map(() => 0),
-  ...splitInput.slice(1)[i].split(" ").map(Number),
-]);
+  const happy: number[] = range(ALL).map(() => 0);
 
-const ALL = 1 << N;
+  const hasBit = (n: number, i: number) => (n & (1 << i)) > 0;
 
-const happy = [...range(ALL)].map(() => 0);
-
-const hasBit = (n: number, i: number) => (n & (1 << i)) > 0;
-
-[...range(ALL)].forEach((n) => {
-  [...range(N)].forEach((i) => {
-    [...range(i + 1, N)].forEach((j) => {
-      if (hasBit(n, i) && hasBit(n, j)) {
-        happy[n] += A[i][j];
-      }
+  range(ALL).forEach((n) => {
+    range(N).forEach((i) => {
+      range(i + 1, N).forEach((j) => {
+        if (hasBit(n, i) && hasBit(n, j)) {
+          happy[n] += A[i][j];
+        }
+      });
     });
   });
-});
 
-let ans = -(10 ** 100);
+  let ans = -(10 ** 100);
 
-[...range(ALL)].forEach((n1) => {
-  [...range(ALL)].forEach((n2) => {
-    if ((n1 & n2) > 0) {
-      return;
-    }
-    const n3 = ALL - 1 - (n1 | n2);
-    ans = Math.max(ans, happy[n1] + happy[n2] + happy[n3]);
+  range(ALL).forEach((n1) => {
+    range(ALL).forEach((n2) => {
+      if ((n1 & n2) > 0) {
+        return;
+      }
+
+      const n3 = ALL - 1 - (n1 | n2);
+
+      ans = Math.max(ans, happy[n1] + happy[n2] + happy[n3]);
+    });
   });
-});
 
-console.log(ans);
+  console.log(ans);
+};
+
+// const input = `3
+// 1 1
+// 1`;
+// export const mainReturn = main(input.split("\n"));
+main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
