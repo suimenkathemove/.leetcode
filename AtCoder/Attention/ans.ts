41e4bca5fca22c71815aeec98ab44d3d1acbebca
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-02 12:15:21 +0900

    Fix Attention

--- AtCoder/Attention/ans.ts
+++ AtCoder/Attention/ans.ts
@@ -1,34 +1,50 @@
-function* range(...args: [number, number] | [number]) {
-  const [start, end] = args.length === 2 ? args : [0, ...args];
+const range = (
+  ...args: [end: number] | [start: number, end: number, step?: number]
+): number[] => {
+  const arr: number[] = [];
 
-  for (let i = start; i < end; i++) {
-    yield i;
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  let i = start;
+  while (step > 0 ? i < end : i > end) {
+    arr.push(i);
+    i += step;
   }
-}
 
-import * as fs from "fs";
+  return arr;
+};
 
-const input = fs.readFileSync("/dev/stdin", "utf8");
+const main = (lines: string[]): void => {
+  const N = Number(lines[0]);
+  const S: ("E" | "W")[] = lines[1].split("") as ("E" | "W")[];
+
+  const sumW = [0];
+  range(N).forEach((i) => {
+    if (S[i] === "W") {
+      sumW.push(sumW[i] + 1);
+    } else {
+      sumW.push(sumW[i]);
+    }
+  });
+
+  let ans = 10 ** 100;
+
+  range(N).forEach((i) => {
+    const w = sumW[i];
+    const e = N - 1 - i - (sumW[N] - sumW[i + 1]);
+
+    ans = Math.min(ans, w + e);
+  });
+
+  console.log(ans);
+};
+
 // const input = `5
 // WEEWW`;
-
-const splitInput = input.split("\n");
-
-const N = Number(splitInput[0]);
-
-const S = splitInput[1].split("") as ("E" | "W")[];
-
-const sumW = [0];
-[...range(N)].forEach((n) => {
-  sumW.push(S[n] === "W" ? sumW[n] + 1 : sumW[n]);
-});
-
-const ans = [...range(N)].reduce((acc, cur) => {
-  const w = sumW[cur];
-  const e = N - 1 - cur - (sumW[N] - sumW[cur + 1]);
-  const turn = w + e;
-
-  return Math.min(turn, acc);
-}, 300000);
-
-console.log(ans);
+// export const mainReturn = main(input.split("\n"));
+main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
--- AtCoder/Attention/my.ts
+++ AtCoder/Attention/my.ts
@@ -1,31 +1,59 @@
-function* range(...args: [number, number] | [number]) {
-  const [start, end] = args.length === 2 ? args : [0, ...args];
+const range = (
+  ...args: [end: number] | [start: number, end: number, step?: number]
+): number[] => {
+  const arr: number[] = [];
 
-  for (let i = start; i < end; i++) {
-    yield i;
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  let i = start;
+  while (step > 0 ? i < end : i > end) {
+    arr.push(i);
+    i += step;
   }
-}
 
-import * as fs from "fs";
+  return arr;
+};
 
-const input = fs.readFileSync("/dev/stdin", "utf8");
-// const input = `5
-// WEEWW`;
+const main = (lines: string[]): void => {
+  const N = Number(lines[0]);
+  const S: ("E" | "W")[] = lines[1].split("") as ("E" | "W")[];
 
-const splitInput = input.split("\n");
+  let ans = 10 ** 100;
 
-const N = Number(splitInput[0]);
+  range(N).forEach((i) => {
+    let sum = 0;
 
-const S = splitInput[1].split("") as ("E" | "W")[];
+    range(N).forEach((j) => {
+      if (i === j) {
+        return;
+      }
 
-const info = [...range(N)].map((n) =>
-  S.reduce(
-    (acc, cur, idx) =>
-      (idx < n && cur === "W") || (n < idx && cur === "E") ? acc + 1 : acc,
-    0
-  )
-);
+      switch (S[j]) {
+        case "E":
+          if (i < j) {
+            sum++;
+          }
+          break;
+        case "W":
+          if (j < i) {
+            sum++;
+          }
+          break;
+      }
+    });
 
-const ans = Math.min(...info);
+    ans = Math.min(ans, sum);
+  });
 
-console.log(ans);
+  console.log(ans);
+};
+
+const input = `5
+WEEWW`;
+export const mainReturn = main(input.split("\n"));
+// main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
