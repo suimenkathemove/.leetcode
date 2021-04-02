8a81244e8b3c4bbe4e250a17eba5e08cc5bf8744
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-02 21:14:28 +0900

    Add range

--- AtCoder/range.ts
+++ AtCoder/range.ts
@@ -31,3 +31,23 @@ function* range(
     yield i;
   }
 }
+
+const range = (
+  func: (i: number, index: number) => void,
+  ...args: [end: number] | [start: number, end: number, step?: number]
+) => {
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  let index = 0;
+
+  for (let i = start; step > 0 ? i < end : i > end; i += step) {
+    func(i, index);
+
+    index++;
+  }
+};
