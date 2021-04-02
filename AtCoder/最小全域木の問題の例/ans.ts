929b0ca3dbc8494b7e98fac64f015982d3093ee0
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-03 01:39:47 +0900

    Fix 最小全域木の問題の例/ans.ts

--- AtCoder/最小全域木の問題の例/ans.ts
+++ AtCoder/最小全域木の問題の例/ans.ts
@@ -1,12 +1,31 @@
-class Heap {
-  constructor(private _data: [number, number][]) {}
+const range = (
+  func: (i: number, index: number) => void,
+  ...args: [end: number] | [start: number, end: number, step?: number]
+) => {
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
 
-  get data() {
-    const data: Readonly<Heap["_data"]> = this._data;
-    return data;
+  let index = 0;
+
+  for (let i = start; step > 0 ? i < end : i > end; i += step) {
+    func(i, index);
+
+    index++;
+  }
+};
+class Heap<T extends [number, number]> {
+  constructor(private _data: T[]) {}
+
+  // @ts-ignore
+  get data(): Readonly<T[]> {
+    return this._data;
   }
 
-  push(item: [number, number]) {
+  push(item: T) {
     this._data.push(item);
   }
 
@@ -14,6 +33,61 @@ class Heap {
     return this._data.sort(([a], [b]) => a - b).shift();
   }
 }
+
+const main = (lines: string[]): void => {
+  const N = Number(lines[0]);
+
+  const graph: [v: number, c: number][][] = [];
+  range((i) => {
+    graph[i] = [];
+  }, N);
+  lines.slice(1).forEach((str) => {
+    const [u, v, c] = str.split(" ").map(Number);
+
+    graph[u].push([v, c]);
+    graph[v].push([u, c]);
+  });
+
+  const marked: boolean[] = [];
+  range((i) => {
+    marked[i] = false;
+  }, N);
+
+  let markedCount = 0;
+
+  const heap = new Heap<[number, number]>([]);
+
+  graph[0].forEach(([i, c]) => {
+    heap.push([c, i]);
+  });
+  marked[0] = true;
+  markedCount++;
+
+  let sum = 0;
+
+  while (markedCount < N) {
+    const [c, i] = heap.pop()!;
+
+    if (marked[i]) {
+      continue;
+    }
+
+    marked[i] = true;
+    markedCount++;
+
+    sum += c;
+
+    graph[i].forEach(([j, c]) => {
+      if (marked[j]) {
+        return;
+      }
+
+      heap.push([c, j]);
+    });
+  }
+
+  console.log(sum);
+};
 
 const input = `5
 0 1 10
@@ -23,73 +97,4 @@ const input = `5
 2 3 30
 2 4 20
 3 4 10`;
-
-const splitInput = input.split("\n");
-
-const N = Number(splitInput[0]);
-
-const G: [number, number][][] = (() => {
-  const arr: [number, number][][] = (() => {
-    const arr: [number, number][][] = [];
-    for (let i = 0; i < N; i++) {
-      arr.push([]);
-    }
-    return arr;
-  })();
-
-  splitInput.slice(1).forEach((s) => {
-    const [ai, bi, c] = s.split(" ").map(Number);
-
-    arr[ai].push([bi, c]);
-    arr[bi].push([ai, c]);
-  });
-
-  return arr;
-})();
-
-const marked: boolean[] = (() => {
-  const arr: boolean[] = [];
-
-  for (let i = 0; i < 5; i++) {
-    arr.push(false);
-  }
-
-  return arr;
-})();
-
-let markCount = 0;
-
-marked[0] = true;
-
-markCount++;
-
-const heap = new Heap([]);
-
-G[0].forEach(([j, c]) => {
-  heap.push([c, j]);
-});
-
-let sum = 0;
-
-while (markCount < N) {
-  const [c, i] = heap.pop();
-
-  if (marked[i]) {
-    continue;
-  }
-
-  marked[i] = true;
-  markCount++;
-
-  sum += c;
-
-  for (const [j, c] of G[i]) {
-    if (marked[j]) {
-      continue;
-    }
-
-    heap.push([c, j]);
-  }
-}
-
-console.log(sum);
+export const mainReturn = main(input.split("\n"));
