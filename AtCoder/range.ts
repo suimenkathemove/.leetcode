fcdcfd1f7f0aefb0c8b5c3907a827cb1a71d3efd
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-03 17:49:49 +0900

    Fix range.ts

--- AtCoder/range.ts
+++ AtCoder/range.ts
@@ -1,22 +1,3 @@
-const range = (
-  ...args: [end: number] | [start: number, end: number, step?: number]
-): number[] => {
-  const arr: number[] = [];
-
-  // @ts-expect-error
-  const [start = 0, end, step = start < end ? 1 : -1]: [
-    number,
-    number,
-    number
-  ] = args.length === 1 ? [void 0, ...args] : args;
-
-  for (let i = start; step > 0 ? i < end : i > end; i += step) {
-    arr.push(i);
-  }
-
-  return arr;
-};
-
 function* range(
   ...args: [end: number] | [start: number, end: number, step?: number]
 ) {
@@ -31,23 +12,3 @@ function* range(
     yield i;
   }
 }
-
-const range = (
-  func: (i: number, index: number) => void,
-  ...args: [end: number] | [start: number, end: number, step?: number]
-) => {
-  // @ts-expect-error
-  const [start = 0, end, step = start < end ? 1 : -1]: [
-    number,
-    number,
-    number
-  ] = args.length === 1 ? [void 0, ...args] : args;
-
-  let index = 0;
-
-  for (let i = start; step > 0 ? i < end : i > end; i += step) {
-    func(i, index);
-
-    index++;
-  }
-};
