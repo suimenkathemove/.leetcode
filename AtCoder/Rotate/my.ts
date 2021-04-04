f2f2f67241e3e2ee84b6a8b24cb33625aab57d46
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-05 00:38:41 +0900

    Fix Rotate/my.ts

--- AtCoder/Rotate/my.ts
+++ AtCoder/Rotate/my.ts
@@ -1,6 +1,9 @@
-const main = (input: string) => {
-  console.log(`${input.slice(1, 3)}${input[0]}`);
+const main = (lines: string[]): void => {
+  const A = lines[0];
+
+  console.log(A.slice(1) + A[0]);
 };
 
-// main(`abc`);
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+// const input = `abc`;
+// export const mainReturn = main(input.split("\n"));
+main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
