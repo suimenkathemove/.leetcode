d78e1e4af53d1c7c19b95988b878a1bf5be1a730
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-05 01:05:32 +0900

    Fix Visibility/my.ts

--- AtCoder/Visibility/my.ts
+++ AtCoder/Visibility/my.ts
@@ -1,8 +1,6 @@
-const range = (
+function* range(
   ...args: [end: number] | [start: number, end: number, step?: number]
-): number[] => {
-  const arr: number[] = [];
-
+) {
   // @ts-expect-error
   const [start = 0, end, step = start < end ? 1 : -1]: [
     number,
@@ -10,60 +8,60 @@ const range = (
     number
   ] = args.length === 1 ? [void 0, ...args] : args;
 
-  let i = start;
-  while (step > 0 ? i < end : i > end) {
-    arr.push(i);
-    i += step;
+  for (let i = start; step > 0 ? i < end : i > end; i += step) {
+    yield i;
+  }
+}
+
+const main = (lines: string[]): void => {
+  let [H, W, X, Y] = lines.splice(0, 1)[0].split(" ").map(Number);
+  H--;
+  W--;
+  X--;
+  Y--;
+  const S = lines.map((str) => str.split(""));
+
+  let ans = 1;
+
+  for (const i of range(X - 1, 0 - 1)) {
+    if (S[i][Y] === "#") {
+      break;
+    }
+
+    ans++;
   }
 
-  return arr;
+  for (const i of range(X + 1, H + 1)) {
+    if (S[i][Y] === "#") {
+      break;
+    }
+
+    ans++;
+  }
+
+  for (const j of range(Y - 1, 0 - 1)) {
+    if (S[X][j] === "#") {
+      break;
+    }
+
+    ans++;
+  }
+
+  for (const j of range(Y + 1, W + 1)) {
+    if (S[X][j] === "#") {
+      break;
+    }
+
+    ans++;
+  }
+
+  console.log(ans);
 };
 
-const main = (input: string) => {
-  const splitInput = input.split("\n");
-
-  const [H, W, X, Y] = splitInput[0].split(" ").map((str) => Number(str) - 1);
-  const S = splitInput.slice(1).map((v) => v.split(""));
-
-  let count = 1;
-
-  if (X < H) {
-    for (const i of range(X + 1, H + 1)) {
-      if (S[i][Y] === "#") {
-        break;
-      }
-      count++;
-    }
-  }
-
-  if (Y < W) {
-    for (const j of range(Y + 1, W + 1)) {
-      if (S[X][j] === "#") {
-        break;
-      }
-      count++;
-    }
-  }
-
-  if (X > 0) {
-    for (const i of range(X - 1, -1)) {
-      if (S[i][Y] === "#") {
-        break;
-      }
-      count++;
-    }
-  }
-
-  if (Y > 0) {
-    for (const j of range(Y - 1, -1)) {
-      if (S[X][j] === "#") {
-        break;
-      }
-      count++;
-    }
-  }
-
-  console.log(count);
-};
-
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+// const input = `4 4 2 2
+// ##..
+// ...#
+// #.#.
+// .#.#`;
+// export const mainReturn = main(input.split("\n"));
+main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
