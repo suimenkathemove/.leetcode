13d4e3a1e0c5a399cd47471ce0633e094135be89
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-06 01:41:52 +0900

    Fix 等比数列/my.ts

--- AtCoder/等比数列/my.ts
+++ AtCoder/等比数列/my.ts
@@ -1,8 +1,6 @@
-const range = (
+function* range(
   ...args: [end: number] | [start: number, end: number, step?: number]
-): number[] => {
-  const arr: number[] = [];
-
+) {
   // @ts-expect-error
   const [start = 0, end, step = start < end ? 1 : -1]: [
     number,
@@ -10,30 +8,28 @@ const range = (
     number
   ] = args.length === 1 ? [void 0, ...args] : args;
 
-  let i = start;
-  while (step > 0 ? i < end : i > end) {
-    arr.push(i);
-    i += step;
+  for (let i = start; step > 0 ? i < end : i > end; i += step) {
+    yield i;
   }
-
-  return arr;
-};
+}
 
 const main = (lines: string[]): void => {
   const [A, R, N] = lines[0].split(" ").map(Number);
 
-  let ans = A;
-
   if (R === 1) {
-    console.log(ans);
+    console.log(A);
+
     return;
   }
 
-  for (let i = 0; i < N - 1; i++) {
+  let ans = A;
+
+  for (const _ of range(N - 1)) {
     ans *= R;
 
     if (ans > 10 ** 9) {
       console.log("large");
+
       return;
     }
   }
@@ -41,6 +37,6 @@ const main = (lines: string[]): void => {
   console.log(ans);
 };
 
-// const input = `2 3 4`;
+// const input = `12 34 5`;
 // export const mainReturn = main(input.split("\n"));
 main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
