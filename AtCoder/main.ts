e2f3e18bc903df65beab47a2d0531fe4a13cbecf
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-05 18:05:50 +0900

    Add range to main.ts

--- AtCoder/main.ts
+++ AtCoder/main.ts
@@ -1,3 +1,18 @@
+function* range(
+  ...args: [end: number] | [start: number, end: number, step?: number]
+) {
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  for (let i = start; step > 0 ? i < end : i > end; i += step) {
+    yield i;
+  }
+}
+
 const main = (lines: string[]): void => {};
 
 const input = ``;
