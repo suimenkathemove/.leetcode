1b15e7bd43c8720aef8bf1de51ea42db35cf110d
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-05 21:59:00 +0900

    Add 山崩し/my.ts

--- AtCoder/山崩し/my.ts
+++ AtCoder/山崩し/my.ts
@@ -15,29 +15,31 @@ function* range(
 
 const main = (lines: string[]): void => {
   const N = Number(lines.splice(0, 1));
-  const S = lines.splice(0, N).map((str) => str.split(""));
+  const S = lines.map((str) => str.split(""));
 
   for (const i of range(N - 1 - 1, 0 - 1)) {
-    for (const j of range(2 * N - 1)) {
-      if (
-        S[i][j] === "#" &&
-        [S[i + 1][j - 1], S[i + 1][j], S[i + 1][j + 1]].some((v) => v === "X")
-      ) {
+    for (const j of range(N - i - 1, 2 * N - 1 - (N - i - 1))) {
+      if ([S[i + 1][j - 1], S[i + 1][j], S[i + 1][j + 1]].includes("X")) {
         S[i][j] = "X";
       }
     }
   }
 
-  S.forEach((sRow) => {
-    console.log(sRow.join(""));
-  });
+  for (const i of range(N)) {
+    console.log(S[i].join(""));
+  }
 };
 
-// const input = `5
-// ....#....
-// ...##X...
-// ..#####..
-// .#X#####.
-// #########`;
+// const input = `10
+// .........#.........
+// ........###........
+// .......#####.......
+// ......#######......
+// .....#########.....
+// ....###########....
+// ...#############...
+// ..###############..
+// .#################.
+// X#X########X#X####X`;
 // export const mainReturn = main(input.split("\n"));
 main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
