14b022d5adc2380a6f2a827cd297d1b636e864eb
yoshiharu2580 <yoshiharu2580@gmail.com>
2021-04-08 03:41:16 +0900

    Fix ハードル走/ans.ts

--- AtCoder/ハードル走/ans.ts
+++ AtCoder/ハードル走/ans.ts
@@ -1,43 +1,33 @@
-const range = (
+function* range(
   ...args: [end: number] | [start: number, end: number, step?: number]
-): number[] => {
-  const arr: number[] = [];
+) {
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
 
-  const [start = 0, end, step = 1] =
-    args.length === 1 ? [void 0, ...args] : args;
-
-  let i = start;
-  while (step > 0 ? i < end : i > end) {
-    arr.push(i);
-    i += step;
+  for (let i = start; step > 0 ? i < end : i > end; i += step) {
+    yield i;
   }
+}
 
-  return arr;
-};
+const main = (inputRows: string[]): void => {
+  const [N, L] = inputRows.splice(0, 1)[0].split(" ").map(Number);
+  const x = inputRows.splice(0, 1)[0].split(" ").map(Number);
+  const [T1, T2, T3] = inputRows[0].split(" ").map(Number);
 
-import * as fs from "fs";
-
-const input = fs.readFileSync("/dev/stdin", "utf8");
-// const input = `10 19
-// 1 3 4 5 7 8 10 13 15 17
-// 2 1000 10`;
-
-const main = () => {
-  const splitInput = input.split("\n");
-
-  const [N, L] = splitInput[0].split(" ").map(Number);
-  const X = splitInput[1].split(" ").map(Number);
-  const [T1, T2, T3] = splitInput[2].split(" ").map(Number);
-
-  const H = range(L + 1).map(() => false);
-  X.forEach((x) => {
-    H[x] = true;
+  const H = [...range(L + 1)].map(() => false);
+  x.forEach((v) => {
+    H[v] = true;
   });
-  const cost = range(L + 1).map(() => 10 ** 100);
+
+  const cost = [...range(L + 1)].map(() => 10 ** 100);
 
   cost[0] = 0;
 
-  range(1, L + 1).forEach((i) => {
+  for (const i of range(1, L + 1)) {
     cost[i] = Math.min(cost[i], cost[i - 1] + T1);
 
     if (i >= 2) {
@@ -45,20 +35,20 @@ const main = () => {
     }
 
     if (i >= 4) {
-      cost[i] = Math.min(cost[i], cost[i - 4] + T1 + 3 * T2);
+      cost[i] = Math.min(cost[i], cost[i - 4] + T1 + T2 * 3);
     }
 
     if (H[i]) {
       cost[i] += T3;
     }
-  });
+  }
 
   let ans = cost[L];
   [L - 3, L - 2, L - 1].forEach((i) => {
     if (i >= 0) {
       ans = Math.min(
         ans,
-        cost[i] + Math.trunc(0.5 * T1) + Math.trunc((L - i - 0.5) * T2)
+        cost[i] + Math.floor(T1 * 0.5) + Math.floor(T2 * (L - i - 0.5))
       );
     }
   });
@@ -66,4 +56,8 @@ const main = () => {
   console.log(ans);
 };
 
-export const ans = main();
+// const input = `4 5
+// 1 2 3 4
+// 2 20 100`;
+// export const mainReturn = main(input.split("\n"));
+main(require("fs").readFileSync("/dev/stdin", "utf8").split("\n"));
