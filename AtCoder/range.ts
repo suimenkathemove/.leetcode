45384765fa3b49dbec439eb75f158bc0fc64b744
Your Name <you@example.com>
2021-04-30 00:43:45 +0900

    Add range

--- AtCoder/range.ts
+++ AtCoder/range.ts
@@ -12,3 +12,24 @@ function* range(
     yield i;
   }
 }
+
+const range = (
+  ...args: [end: number] | [start: number, end: number, step?: number]
+): number[] => {
+  const arr: number[] = [];
+
+  // @ts-expect-error
+  const [start = 0, end, step = start < end ? 1 : -1]: [
+    number,
+    number,
+    number
+  ] = args.length === 1 ? [void 0, ...args] : args;
+
+  let i = start;
+  while (step > 0 ? i < end : i > end) {
+    arr.push(i);
+    i += step;
+  }
+
+  return arr;
+};
